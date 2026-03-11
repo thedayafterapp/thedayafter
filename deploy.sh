a5ae06a3c63ef6a84166a5c7b3c7f30393f87a12
thedayafterapp <thedayafterapp@gmail.com>
2026-03-11 19:36:05 +0100

    test

--- deploy.sh
+++ deploy.sh
@@ -17,8 +17,8 @@ echo "==> Restarting app container..."
 docker compose up -d --force-recreate php
 
 echo "==> Running migrations & cache inside container..."
-docker compose exec -T php php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
-docker compose exec -T php php bin/console cache:warmup --no-interaction
+docker compose exec -T -u www-data php php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
+docker compose exec -T -u www-data php php bin/console cache:warmup --no-interaction
 
 echo ""
 echo "==> Deploy complete!"
