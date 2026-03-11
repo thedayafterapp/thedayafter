4aa67642566073864c88f909c3a254d1de899ac8
thedayafterapp <thedayafterapp@gmail.com>
2026-03-11 17:50:28 +0100

    add demo

---
+++ app/src/Controller/DemoController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DemoController extends AbstractController
+{
+    #[Route('/demo', name: 'app_demo')]
+    public function index(): Response
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_dashboard');
+        }
+
+        return $this->render('demo/index.html.twig');
+    }
+}
