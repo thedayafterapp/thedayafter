1ec8a6ef78e5c56020ea7e16a4550e5357ca5456
thedayafterapp <thedayafterapp@gmail.com>
2026-03-11 01:32:07 +0100

    add button to reset achievements

--- app/src/Controller/AchievementsController.php
+++ app/src/Controller/AchievementsController.php
@@ -3,10 +3,15 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Entity\UserAchievement;
 use App\Repository\AchievementRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Csrf\CsrfToken;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 
 class AchievementsController extends AbstractController
 {
@@ -27,5 +32,27 @@ class AchievementsController extends AbstractController
             'achievements' => $all,
             'earned' => $earned,
         ]);
+    }
+
+    #[Route('/achievements/reset', name: 'app_achievements_reset', methods: ['POST'])]
+    public function reset(
+        Request $request,
+        EntityManagerInterface $em,
+        CsrfTokenManagerInterface $csrf,
+    ): Response {
+        /** @var User $user */
+        $user = $this->getUser();
+
+        if (!$csrf->isTokenValid(new CsrfToken('reset_achievements', $request->request->get('_token')))) {
+            throw $this->createAccessDeniedException('Invalid CSRF token.');
+        }
+
+        foreach ($user->getUserAchievements() as $ua) {
+            $em->remove($ua);
+        }
+        $em->flush();
+
+        $this->addFlash('success', 'All achievements reset.');
+        return $this->redirectToRoute('app_achievements');
     }
 }
